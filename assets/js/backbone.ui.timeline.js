da230682000ffd9b88df7085167322a8cf8728d8
Makis Tracend <makis.tracend@gmail.com>
2013-04-06 02:12:10 -0700

    improving how the lib is initialized

--- assets/js/backbone.ui.timeline.js
+++ assets/js/backbone.ui.timeline.js
@@ -1,34 +1,48 @@
-Backbone.UI = {};
+// Backbone.js Timeline extension
+//
+// Created by: Lyndel Thomas (@tracend)
+// Source: https://github.com/backbone-ui/timeline
+//
+// Licensed under the MIT license: 
+// http://makesites.org/licenses/MIT
 
-Backbone.UI.Timeline = Backbone.View.extend({
+(function(_, Backbone) {
 	
-	initialize: function( options ){
-		var self = this;
-		this.data = ( options.data ) ? options.data : [];
-		// 
-		this.$timeline = $(this.el).find('input');
-		this.$timelineDate = $(this.el).find('.date');
-		this.dataCount = this.data.length;
-		this.timelineWidth = this.$timeline.width();
-		this.step =  this.timelineWidth / (this.dataCount-1);
-			
-		// set the  max and value in timeline
-		this.$timeline
-			.attr('value', 1)
-			.attr('max', this.dataCount);
-			
-		setTimeout(function(){
-			self.$timeline.trigger("change");
-		}, 500);
-	},
+	// fallbacks
+	if( _.isUndefined( Backbone.UI ) ) Backbone.UI = {};
 	
-	events: {
-		"change input" : "updateTimelineDate"
-	},
-	
-	updateTimelineDate: function( e ) {
-		var val = $(e.target).val();
-		this.$timelineDate.html(this.data[val-1]);
-		this.$timelineDate.css("left", (val-1) * this.step +"px" );
-	}
-});
+	Backbone.UI.Timeline = Backbone.View.extend({
+		
+		initialize: function( options ){
+			var self = this;
+			this.data = ( options.data ) ? options.data : [];
+			// 
+			this.$timeline = $(this.el).find('input');
+			this.$timelineDate = $(this.el).find('.date');
+			this.dataCount = this.data.length;
+			this.timelineWidth = this.$timeline.width();
+			this.step =  this.timelineWidth / (this.dataCount-1);
+				
+			// set the  max and value in timeline
+			this.$timeline
+				.attr('value', 1)
+				.attr('max', this.dataCount);
+				
+			setTimeout(function(){
+				self.$timeline.trigger("change");
+			}, 500);
+		},
+		
+		events: {
+			"change input" : "updateTimelineDate"
+		},
+		
+		updateTimelineDate: function( e ) {
+			var val = $(e.target).val();
+			this.$timelineDate.html(this.data[val-1]);
+			this.$timelineDate.css("left", (val-1) * this.step +"px" );
+		}
+	});
+
+
+})(this._, this.Backbone);
