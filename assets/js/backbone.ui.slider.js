f5c9a2dc537a0872ab0f6d812ac0363a9f2affb3
Makis Tracend <makis.tracend@gmail.com>
2014-07-21 23:58:50 -0700

    supporting APP and module loaders

--- assets/js/backbone.ui.slider.js
+++ assets/js/backbone.ui.slider.js
@@ -1,42 +1,47 @@
-// Backbone.js Slider extension
-//
-// Created by: Lyndel Thomas (@ryndel)
-// Source: https://github.com/backbone-ui/timeline
-//
-// Licensed under the MIT license: 
-// http://makesites.org/licenses/MIT
+/*
+ * Backbone UI: Slider
+ * Source: https://github.com/backbone-ui/slider
+ * Copyright © Makesites.org
+ *
+ * Initiated by Lyndel Thomas (@ryndel)
+ * Distributed through [Makesites.org](http://makesites.org)
+ * Released under the [MIT license](http://makesites.org/licenses/MIT)
+ */
 
-(function(_, Backbone) {
-	
-	// fallbacks
-	if( _.isUndefined( Backbone.UI ) ) Backbone.UI = {};
-	
-	Backbone.UI.Slider = Backbone.View.extend({
-		
+(function(window, $, _, Backbone, APP) {
+
+	// support for Backbone APP() view if available...
+	var isAPP = ( typeof APP !== "undefined" && typeof APP.View !== "undefined" );
+	var View = ( isAPP ) ? APP.View : Backbone.View;
+
+
+	var Slider = View.extend({
+
 		initialize: function( options ){
 			var self = this;
 			this.data = ( options.data ) ? options.data : [];
-			// 
+			//
 			this.$slider = $(this.el).find('input');
 			this.$sliderDate = $(this.el).find('.date');
 			this.dataCount = this.data.length;
 			this.timelineWidth = this.$slider.width();
 			this.step =  this.timelineWidth / (this.dataCount-1);
-				
+
 			// set the  max and value in timeline
 			this.$slider
 				.attr('value', 1)
 				.attr('max', this.dataCount);
-				
+
+			// trigger input once on init
 			setTimeout(function(){
 				self.$slider.trigger("change");
 			}, 500);
 		},
-		
+
 		events: {
 			"change input" : "updateSliderDate"
 		},
-		
+
 		updateSliderDate: function( e ) {
 			var val = $(e.target).val();
 			this.$sliderDate.html(this.data[val-1]);
@@ -45,4 +50,31 @@
 	});
 
 
-})(this._, this.Backbone);
+	// Support module loaders
+	if ( typeof module === "object" && module && typeof module.exports === "object" ) {
+		// Expose as module.exports in loaders that implement CommonJS module pattern.
+		module.exports = Slider;
+	} else {
+		// Register as a named AMD module, used in Require.js
+		if ( typeof define === "function" && define.amd ) {
+			//define("backbone.ui.slider", ['jquery''underscore', 'backbone'], function(){ return Slider; });
+		}
+	}
+	// If there is a window object, that at least has a document property
+	if ( typeof window === "object" && typeof window.document === "object" ) {
+		// update APP namespace
+		if( isAPP ){
+			APP.UI = APP.UI || {};
+			APP.UI.Slider = Slider;
+			// save namespace
+			window.APP = APP;
+		}
+		// update Backbone namespace regardless
+		Backbone.UI = Backbone.UI || {};
+		Backbone.UI.Slider = Slider;
+		window.Backbone = Backbone;
+	}
+
+
+
+})(this.window, this.$, this._, this.Backbone, this.APP);
